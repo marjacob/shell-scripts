c028209193e27f1eee322b06e474a33eb864196e
Martin Røed Jacobsen <martin@ifi.uio.no>
2015-09-19 17:13:44 +0200

    Added initial version
    The setup script currently supports a number of basic features
    
    - Checking whether or not the user running it is root
    
    - Setting the system hostname
    
    - Configuring the system time
    
    - Updating and installing packages
    
    - Configuring a basic firewall
    
    - Creating users and granting sudo permissions

--- setup-debian-8.sh
+++ setup-debian-8.sh
@@ -1,1 +1,81 @@
-Test
+#!/usr/bin/env bash
+
+# Written with Debian 8 in mind.
+
+conf_hostname="shion"
+conf_timezone="Europe/Oslo"
+conf_users="martin;terje"
+conf_sudoers="martin"
+conf_packages="
+	build-essential \
+	gdb \
+	git \
+	mosh \
+	sudo \
+	tmux \
+	tree \
+	ufw \
+	valgrind \
+	vlock"
+
+# Suppress requests for information during package configuration.
+export DEBIAN_FRONTENT=noninteractive
+
+# Determines whether a program is available or not.
+function has {
+	command -v "$@" >/dev/null 2>&1	
+}
+
+# Make sure that the user is actually root.
+if [[ $EUID -ne 0 ]]; then
+	printf "error: must be run as root\n"
+	exit 1;
+fi
+
+# Configure the system hostname.
+# -----------------------------------------------------------------------------
+
+if has hostnamectl; then
+	hostnamectl set-hostname "$conf_hostname"
+else
+	echo "${conf_hostname}" > /etc/hostname
+	hostname -F /etc/hostname
+fi
+
+# Configure the system time zone.
+# -----------------------------------------------------------------------------
+
+echo "$conf_timezone" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+# Update the system and install new packages.
+# -----------------------------------------------------------------------------
+
+aptitude update
+aptitude upgrade -y
+aptitude -y install ${conf_packages}
+
+# Configure and enable the firewall.
+# -----------------------------------------------------------------------------
+
+ufw default deny incoming
+ufw default allow outgoing
+ufw limit 60001/udp
+ufw limit ssh/tcp
+ufw enable
+
+# Create and configure user accounts.
+# -----------------------------------------------------------------------------
+
+# Create users.
+IFS=';' read -ra ADDR <<< "${conf_users}"
+for i in "${ADDR[@]}"; do
+	adduser --disabled-password --gecos "" "${i}"
+done
+
+# Grant sudo permissions to specified users.
+IFS=';' read -ra ADDR <<< "${conf_sudoers}"
+for i in "${ADDR[@]}"; do
+	usermod -a -G sudo "${i}"
+done
+
