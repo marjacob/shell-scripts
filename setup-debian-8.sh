8aa42335c87e74032d5478cc4fbf27fe7fb72162
Martin <marjacob@users.noreply.github.com>
2015-12-07 21:17:20 +0100

    Changed instructions to use firewall-cmd

--- setup-debian-8.sh
+++ setup-debian-8.sh
@@ -329,7 +329,7 @@ printf "\n"`
 	`"   2) Change PasswordAuthentication to \"no\".\n"`
 	`"   3) Restart the SSH service:\n"`
 	`"      sudo systemctl restart sshd\n"`
-	`" o Use UFW to manage the firewall (man ufw).\n"`
+	`" o Use firewalld to manage the firewall (man firewall-cmd).\n"`
 	`"\n"
 
 exit 0
