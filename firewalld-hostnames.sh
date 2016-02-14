4acdbb5308799683dbbba4768f4700a07a76c553
Martin Røed Jacobsen <marjacob@users.noreply.github.com>
2016-02-14 16:11:03 +0100

    Shortened the script considerably

--- firewalld-hostnames.sh
+++ firewalld-hostnames.sh
@@ -16,16 +16,10 @@ cfg_hostnames=(
 # Internal functions and variables.
 # -----------------------------------------------------------------------------
 
-function fw_allow_ipv4 {
+function fw_allow_ip {
 	firewall-cmd \
 		--zone=${cfg_firewall_zone} \
-		--add-source=${1}/32 >/dev/null 2>&1
-}
-
-function fw_allow_ipv6 {
-	firewall-cmd \
-		--zone=${cfg_firewall_zone} \
-		--add-source=${1}/64 >/dev/null 2>&1
+		--add-source=${1} >/dev/null 2>&1
 }
 
 function fw_reload {
@@ -42,22 +36,12 @@ fw_reload
 # Process all configured hostnames.
 if [ ${#cfg_hostnames[@]} -gt 0 ]; then
 	for hostname in "${cfg_hostnames[@]}"; do
-		# Allow each IPv4 address pointed to by the hostname.
-		ipv4_address_list=$(dig "${hostname}" A +short)
-		for ipv4_address in ${ipv4_address_list}; do
+		ip_addresses=$(dig "${hostname}" A "${hostname}" AAAA +short)
+		for ip_address in ${ip_addresses}; do
 			printf "allow %-39s %s\n" \
-				"${ipv4_address}" \
+				"${ip_address}" \
 				"${hostname}"
-			fw_allow_ipv4 "${ipv4_address}"
-		done
-
-		# Allow each IPv6 address pointed to by the hostname.
-		ipv6_address_list=$(dig "${hostname}" AAAA +short)
-		for ipv6_address in ${ipv6_address_list}; do
-			printf "allow %-39s %s\n" \
-				"${ipv6_address}" \
-				"${hostname}"
-			fw_allow_ipv6 "${ipv6_address}"
+			fw_allow "${ip_address}"
 		done
 	done
 fi
